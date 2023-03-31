ed27114959e2c82d83ede69263b1272060ec2558
Tara Mazaya Lababan <tara.mazaya@ui.ac.id>
2023-04-01 05:02:27 +0700

    commit

--- frontend/app.py
+++ frontend/app.py
@@ -6,9 +6,7 @@ from fastapi.templating import Jinja2Templates
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="templates/static"), name="static")
 
-
 templates = Jinja2Templates(directory="templates")
-
 
 @app.get("/", response_class=HTMLResponse)
 async def index(request: Request):
